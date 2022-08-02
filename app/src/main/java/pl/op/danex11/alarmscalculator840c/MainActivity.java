4898c430dd49a8792d3cb2854c500e8b697f6454
danex11 <danex11@op.pl>
2022-08-02 13:51:45 +0200

    TODO 50 group TODO @bug email address TODO @eds identyficator TODO show keyboard automatically :/ 20220802

--- app/src/main/java/pl/op/danex11/alarmscalculator840c/MainActivity.java
+++ app/src/main/java/pl/op/danex11/alarmscalculator840c/MainActivity.java
@@ -1,80 +1,71 @@
 package pl.op.danex11.alarmscalculator840c;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdError;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
 
     boolean initializationDone = false;
-    String TAG = "tag";
     private RewardedAd mRewardedAd;
     //private
     int adCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //showKeyboard();
+        Log.w("logtag", "Log");
+      //  Log.v("lineseparatoris", System.getProperty("line.separator"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator);
+        //Alarm enter view
+        EditText alrmtyped = (EditText) findViewById(R.id.alarmtyped);
+        //alrmtyped.setFocusableInTouchMode(true);
+        alrmtyped.setInputType(InputType.TYPE_CLASS_NUMBER);
+       // alrmtyped.requestFocus();
+        Log.w("logtag", "requestfocus");
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        Log.w("logtag", "imm" + imm);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.SHOW_IMPLICIT);
+        // imm.showSoftInput(alrmtyped,InputMethodManager.SHOW_IMPLICIT);
+
+
+
+
         // retain this fragment
+        FrameLayout framelayout = findViewById(R.id.framelayout);
+        framelayout.setTranslationZ(-10);
 
-       // AdView adView = new AdView(this);
-        //adView.setAdSize(AdSize.BANNER);
-       // adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-// TODO: Add adView to your view hierarchy.
-        /*
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-// Place the ad view.
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        //LinearLayout adContainer = <container>;
-        adContainer.addView(adView, params);
+        final String[] result = {"waiting"};
+        // your text box
+       // showSoftKeyboard(alrmtyped);
 
-        // initialize the Google Mobile Ads SDK
-        // initializes the SDK and calls back a completion listener once initialization is complete (or after a 30-second timeout).
-        // This needs to be done only once, ideally at app launch.
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-                initializationDone = true;
-                Log.e(TAG, "initializationDone = true Set");
-                //loadad loads rewardedAd
-               // loadAd();
 
-            }
-        });
-         */
+        //AD    AD  AD
         // AdMob 1 : add view
         AdView mAdView = (AdView) findViewById(R.id.adView);
         //AdMob 2 : initialize
@@ -87,47 +78,26 @@ public class MainActivity extends AppCompatActivity {
         //AdMob 3 : request and load an ad
         AdRequest adRequest = new AdRequest.Builder()
                 .build();
-        mAdView.loadAd(adRequest);
+        //mAdView.loadAd(adRequest);
 
-        /*
-            //set fullscreen callback
-            mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-                @Override
-                public void onAdShowedFullScreenContent() {
-                    // Called when ad is shown.
-                    Log.e(TAG, "Ad was shown.");
-                }
 
-                @Override
-                public void onAdFailedToShowFullScreenContent(AdError adError) {
-                    // Called when ad fails to show.
-                    Log.e(TAG, "Ad failed to show.");
-                }
-
-                @Override
-                public void onAdDismissedFullScreenContent() {
-                    // Called when ad is dismissed.
-                    // Set the ad reference to null so you don't show the ad a second time.
-                    Log.e(TAG, "Ad was dismissed.");
-                    mRewardedAd = null;
-                }
-            });
-*/
-        final String[] result = {"waiting"};
-        // your text box
-        EditText alrmtyped = (EditText) findViewById(R.id.alarmtyped);
-
+        //ENTER     ENTER   ENTER   ENTER
         alrmtyped.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 //CLICKED Enter
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-
-                    CalculateAlarm calca = new CalculateAlarm(alrmtyped.getText().toString());
+                    String alarm = checkAlarmErrors(alrmtyped.getText().toString());
+                    //vvvvvvvvvvvv
+                    CalculateAlarm calca = new CalculateAlarm(alarm);
+                    //show ad
+                    mAdView.loadAd(adRequest);
+                    //^^^^^^^^^^^^
                     result[0] = calca.getDbAddress();
                     EditText resultETxt = findViewById(R.id.dbbresult);
                     resultETxt.setText(result[0]);
-                    closeKeyboard();
+                    //closeKeyboard();
+                    //hideSoftKeyboard(alrmtyped);
                     return true;
                 }
                 return false;
@@ -137,61 +107,80 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    public void hideSoftKeyboard(View view){
+        InputMethodManager imm =(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 
-    //AD
-    void loadAd() {
-        // IF (){}
-        if (initializationDone == true) {
-            Log.e(TAG, "initializationDone == true");
-            //load rewarded ad object
-            AdRequest adRequest = new AdRequest.Builder().build();
-
-            RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
-                    adRequest, new RewardedAdLoadCallback() {
-                        @Override
-                        public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                            // Handle the error.
-                            Log.e(TAG, loadAdError.getMessage());
-                            mRewardedAd = null;
-                        }
-
-                        @Override
-                        public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
-                            mRewardedAd = rewardedAd;
-                            Log.e(TAG, "Ad was loaded.");
-                            showAd();
-                        }
-                    });
+    public void showSoftKeyboard(View view){
+        Log.w("logtag", "showkeyboard");
+        view.requestFocus();
+        if(view.requestFocus()){
+            Log.w("logtag", "requestfocus");
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            Log.w("logtag", "imm" + imm);
+            imm.showSoftInput(view,InputMethodManager.SHOW_IMPLICIT);
         }
     }
-    void showAd(){
-        //show the ad
-        Log.e(TAG, "Show Ad block started");
-        if (mRewardedAd != null) {
-            Log.e(TAG, "mRewardedAd != null inside \"show the ad\"");
-            Activity activityContext = MainActivity.this;
-            mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
-                @Override
-                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
-                    // Handle the reward.
-                    Log.e(TAG, "The user earned the reward.");
-                    int rewardAmount = rewardItem.getAmount();
-                    String rewardType = rewardItem.getType();
-                    adCounter++;
-                }
-            });
-        } else { Log.e(TAG, "The rewarded ad wasn't ready yet."); }
 
+
+
+
+    public String checkAlarmErrors(String alarmtyped){
+        Log.w("logtag","typed " +  alarmtyped);
+        boolean wrongAlert = false;
+
+        String digs12 = alarmtyped.substring(0,2);
+        Log.w("logtag","digs12 " +  digs12);
+        if (!digs12.equals("50") && !digs12.equals("51") && !digs12.equals("52") && !digs12.equals("53") && !digs12.equals("54") && !digs12.equals("55") && !digs12.equals("56") && !digs12.equals("57") && !digs12.equals("58")
+                && !digs12.equals("60") && !digs12.equals("70"))
+        { Toast.makeText(MainActivity.this, "Alarm - first two digits wrong.", Toast.LENGTH_LONG).show();
+        wrongAlert = true;}
+
+        String dig3 = alarmtyped.substring(2,3);
+        Log.w("logtag","dig3 " +  dig3);
+        //System.out.print(dig3);
+        if(!dig3.equals("0") && !dig3.equals("1") && !dig3.equals("2") && !dig3.equals("3"))
+        { Toast.makeText(MainActivity.this, "Alarm - mistaken third digit.", Toast.LENGTH_LONG).show();
+        wrongAlert = true; }
+
+        String digs34 = alarmtyped.substring(2,4);
+        Log.w("logtag","digs34 " +  digs34);
+        if(     (digs12.equals("70") && Integer.parseInt(digs34)>31)
+            ||
+                (digs12.equals("60") && Integer.parseInt(digs34)>18)
+            ||
+                digs12.equals("50") && !digs34.equals("01") && !digs34.equals("02") && !digs34.equals("03") && !digs34.equals("11") && !digs34.equals("12") && !digs34.equals("13") )
+        { Toast.makeText(MainActivity.this, "Alarm - third/fourth digits incorrect.", Toast.LENGTH_LONG).show();
+        wrongAlert = true; }
+
+        String digs56 = alarmtyped.substring(4,6);
+        Log.w("logtag","digs56 " +  digs56);
+        if(     (digs12.equals("70") && Integer.parseInt(digs56)>31)
+                    ||
+                    (digs12.equals("60") && Integer.parseInt(digs56)>15)
+                    ||
+                    digs12.equals("50") && Integer.parseInt(digs56)>63 )
+        { Toast.makeText(MainActivity.this, "Alarm - fifth and sisth digit too high.", Toast.LENGTH_LONG).show();
+        wrongAlert = true; }
+
+        if (wrongAlert){alarmtyped = "000000"; }
+        if (alarmtyped!="000000"){
+            Toast.makeText(MainActivity.this, "OK!.", Toast.LENGTH_LONG).show();
+        }
+
+        return alarmtyped;
     }
-
-
-
 
 
 
 
     public void loadBug(View view){
-
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","email@email.com", null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "@string/emsubject");
+        intent.putExtra(Intent.EXTRA_TEXT, "@string/emmessage");
+        startActivity(Intent.createChooser(intent, "Choose an Email client :"));
     }
 
 
@@ -202,6 +191,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void loadInfo(View view) {
+        FrameLayout framelayout = findViewById(R.id.framelayout);
+        framelayout.setTranslationZ(+10);
+        LayoutInflater inflater = (LayoutInflater) Objects.requireNonNull(getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+        View viewL  =
+                inflater.inflate(R.layout.infolayout,framelayout,false);
+        framelayout.addView(viewL);
+    }
+
+    public void removeInfo(View view) {
+        FrameLayout framelayout = findViewById(R.id.framelayout);
+        framelayout.setTranslationZ(-10);
+        framelayout.removeAllViews();
+    }
 
 
     private void closeKeyboard()
@@ -232,9 +235,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     @Override
     protected  void onResume(){
         super.onResume();
+        EditText alrmtyped = (EditText) findViewById(R.id.alarmtyped);
+
+
+        //TODO hide keyboard, load new ad
 //        //Layout params
 //        RelativeLayout layout = new RelativeLayout(this);
 //        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
@@ -275,6 +283,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 
 
 }
